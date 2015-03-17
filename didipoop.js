1663a8be7f02e9c1752a691fc75ddd35638d6b8d
limingth <limingth@gmail.com>
2015-03-17 11:46:27 +0800

    Initial commit of didipoop using meteor

---
+++ didipoop.js
@@ -0,0 +1,30 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    toomanypoop: function () {
+      // body...
+      if (Session.get('counter') > 3)
+        return true;
+      else
+        return false;
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
